37dfea3d69e13a7bef926a2db7c5db7bba3ea9af
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-08-17 22:57:10 +0530

    added new class containing examples

--- DifferentBlocks.java
+++ DifferentBlocks.java
@@ -5,16 +5,28 @@
  * 
  * Instance Block will be exicuted at the time of object creation so it will
  * exicuted after main method ..
+ * 
+ * Constructor will be exicuted at the time of object reation , a class can have
+ * any number of constructor ..
+ * The name of the constructor must be same as the name of Class
+ * 
+ * Methods are also called ase block , a method can either be static or instance
+ * 
+ * 
  */
-public class Demo {
+public class DifferentBlocks {
     static {
         System.out.println("In static block outside main method " + '\n');
+    }
+
+    DifferentBlocks() {
+        System.out.println("In Constructor  main method " + '\n');
     }
 
     public static void main(String[] args) {
         System.out.print("In main method " + '\n');
 
-        Demo demo = new Demo();
+        // Demo demo = new Demo();
 
     }
 
---
+++ MethodOverloadingEX.java
@@ -0,0 +1,35 @@
+/**
+ * MethodOverloadingEX
+ * 
+ * It is a process of sepcifing multiple method having same name and different
+ * mehod signature (Which is different number of Parameters , type of Parametes
+ * and order of Parameters)
+ * 
+ * Method Over loading can be done in same class or in two different class
+ * having IS-A Realtionship(Parrent Child Relationship/Inhertance)
+ */
+class MethodOverloadingEX {
+
+    void methodOne(int a) {
+        System.out.println("In Parrent class methodOne");
+    }
+
+}
+
+class MethodOverloadingEXChild extends MethodOverloadingEX {
+    void methodTwo(double a) {
+        System.out.println("In Child class methodTwo");
+    }
+
+    public static void main(String[] args) {
+        // Using parrent class object we only can access parrent class methods
+        MethodOverloadingEX parrent = new MethodOverloadingEX();
+        parrent.methodOne(1);
+
+        // Using child class object we can access both parrent and child class methods
+        MethodOverloadingEXChild child = new MethodOverloadingEXChild();
+        child.methodOne(2);
+        child.methodTwo(3.4);
+
+    }
+}

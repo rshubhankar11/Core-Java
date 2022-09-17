23d582963f10076179648ad39ff57308c3a73ab7
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-09-17 18:13:09 +0530

    added new interview asked program

---
+++ InterviewPrograms/InfosysInterviewQ.java
@@ -0,0 +1,69 @@
+import java.nio.channels.ClosedSelectorException;
+import java.util.Arrays;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import org.w3c.dom.css.Counter;
+
+/**
+ * InfosysInterviewQ
+ * 
+ * 
+ * 
+ * Write a function:
+ * class Solution { public int solution(int[] A); }
+ * that, given an array A of N integers, returns the smallest positive integer
+ * (greater than 0) that does not occur in A.
+ * For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+ * Given A = [1, 2, 3], the function should return 4.
+ * Given A = [−1, −3], the function should return 1.
+ */
+
+public class InfosysInterviewQ {
+
+    /**
+     * This method will return the smallest posetive number which is not present in
+     * the array
+     * 
+     * @param array
+     * @return
+     */
+    public static int solution(int[] a) {
+        // sorting the array
+        Arrays.sort(a);
+        // Smallest element of the array
+        int counter = a[0];
+        int counterNegative = 1;
+        // For posetive numbers
+        for (int i = 0; i < a.length; i++) {
+            if (counter == a[i]) {
+                counter = counter + 1;
+            }
+        }
+        // in case when smallest number which is missing in our array is a negative
+        // number
+        if (counter < 0) {
+            for (int i = 0; i < a.length; i++) {
+                if (counterNegative == a[i]) {
+                    counterNegative = counterNegative + 1;
+                }
+            }
+            return counterNegative;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        // int[] A = new int[] { 1, 3, 6, 4, 1, 2 };
+        // int[] A = new int[] { 1, 2, 3 };
+        int[] A = new int[] { -1, -3 };
+
+        // int[] A = new int[] { -10000, 10000, 1, 999, 2 };
+
+        // int[] A = new int[] { 71, 72, 73, 1, 2, 3, 4 };
+
+        int solution = solution(A);
+        System.out.println(solution);
+    }
+
+}
